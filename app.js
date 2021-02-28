194be23207ac22cb93b87b9ec1d5990f3ccb7046
Eli <elit1989+1@gmail.com>
2021-02-28 21:40:46 +0200

    styling and markup changed a bit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,6 +10,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 

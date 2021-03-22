8ad8b59179483b775a0f7f17a42f33f749df3b41
Eli <elit1989+1@gmail.com>
2021-03-22 23:05:39 +0200

    clean up done settled on ejs

---
+++ controllers/error.js
@@ -0,0 +1,8 @@
+exports.get404 = (req, res, next) => {
+    res.status(404).render('404', {
+        pageTitle: 'Page not found', path: '404',
+        formsCss: false,
+        productCss: false,
+        activeAddProduct: false
+    });
+};
